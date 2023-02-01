4a6dbd5c7bf356e4ffcd8467ef290070b35bd1e4
alvin C <alvinchristianto93@gmail.com>
2023-02-02 04:55:35 +0700

    add github pages

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>
+         Ready to use #2!!
+        </h3>
         <a
           className="App-link"
           href="https://reactjs.org"
